2c3e7f8a920422f5df9e22ec41eeb0b8d7b2801e
Fernanda <fecka1@gmail.com>
2020-09-29 15:48:30 -0300

    Primeiro commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/dashboard">Dashboard</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>

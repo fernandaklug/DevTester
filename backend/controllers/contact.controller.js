d01771937a5ea285dd45fc2b995951096a102c84
Fernanda <fecka1@gmail.com>
2020-10-06 00:09:17 -0300

    Banco de Dados + Testes Unitários

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,24 +1,10 @@
+const ContactModel = require('../models/contact.model')
+
+
+
 module.exports = {
-    list(resquest, h){
-        return [
-            {
-                id: 1,
-                name: "Fernanda",
-                number: "11 999999999",
-                description: "Solicitar consultoria em DevOps",
-              },
-              {
-                id: 2,
-                name: "João da Silva",
-                number: "11 999999999",
-                description: "Orçamento para aulas de inglês",
-              },
-              {
-                id: 3,
-                name: "Maria da Silva",
-                number: "11 999999999",
-                description: "Orçamento para aulas de francês",
-              },
-        ]
+    async list(resquest, h){
+        const contacts = await ContactModel.find().exec();
+        return contacts
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,49 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const mongoose = require('mongoose');
+
+const mongoURL = "mongodb+srv://qaninja:qaninja@cluster0.jxp29.gcp.mongodb.net/zaplinkdb?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB Conneted');
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB Error: ' + error);
+})
 
 const contactRoutes = require('./routes/contact.routes')
 
-const init = async () => {
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return { message: 'Welcome to ZapLink API', company: 'QA Ninja', course: 'DevTester' };
+    }
+});
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+server.route(contactRoutes)
 
-            return {message: 'welcome to Zaplink API', company: 'QA Ninja', course: 'DevTester'}
-        }
-    });
-
-    server.route(contactRoutes)
-    
-    await server.start();
+server.start((err)=> {
+    if(err) {
+        throw err;
+    }
     console.log('Server running on %s', server.info.uri);
-};
+});
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err);
     process.exit(1);
 });
 
-init();
+exports.init = async () => {
+    return server;
+}
